f92dcc95ce33f55f2d1567245b1fa3b5abf3cac1
Alejo <alejandrocantillop.ilusa@gmail.com>
2025-06-06 12:02:11 -0500

    issues

--- src/app/components/main-view/CategoriesWrapper.jsx
+++ src/app/components/main-view/CategoriesWrapper.jsx
@@ -4,15 +4,13 @@ import Categories from '@/app/components/main-view/Categories';
 import Products from '@/app/components/main-view/Products';
 
 const CategoriesWrapper = ({ products, categories }) => {
-  const [categoriesData, setCategoriesData] = useState(categories);
   const [selectedCategory, setSelectedCategory] = useState('Todos');
-
   return (
     <>
       <Categories
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}
-        categories={categoriesData}
+        categories={categories}
       />
       <Products products={products} selectedCategory={selectedCategory} />
     </>
